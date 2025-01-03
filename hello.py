1bf405011f25be2ac262b6b22d48d3af3c60cc0b
waseem-jadaa <rowaid.jadaa@gmail.com>
2025-01-03 16:44:23 +0200

    first python code

---
+++ hello.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+print("calculator")
+num1=input("enter first number : ")
+num2=input("enter second number : ")
+sum=float(num1)+float(num2)
+subtract=float(num1)-float(num2)
+multiple=float(num1)*float(num2)
+devide=float(num1)/float(num2)
+sqrt_root1=sqrt(float(num1))
+sqrt_root2=sqrt(float(num2))
+print("square number1 : "+str(sqrt_root1))
+print("square number2 :"+str(sqrt_root2))
+print("the sum: "+str(sum))
+print("the substract : "+str(subtract))
+print("the multiple : "+str(multiple))
+print("the devide : "+str(devide))
